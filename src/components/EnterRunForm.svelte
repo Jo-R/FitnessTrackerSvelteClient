e1abd0be17cd67c99d5f02a278bba489b4c15310
Jo-R <jo.rigg8@gmail.com>
2021-01-05 20:22:00 +0000

    start styling add run form

--- src/components/EnterRunForm.svelte
+++ src/components/EnterRunForm.svelte
@@ -16,7 +16,7 @@
     try {
       const dateTime = new Date(`${date}T${time}`);
       const response = await addRunActivity({
-        userId: "d30e52b0-304c-4aa1-3c68-08d888b124c0",
+        userId: "d30e52b0-304c-4aa1-3c68-08d888b124c0", //TODO once have login remove hard coded
         date: dateTime,
         title,
         distanceMile: distance,
@@ -25,9 +25,9 @@
         averagePace: `00:${avgPaceMin}:${avgPaceSec}`,
         notes,
       });
-      console.log(response);
+      console.log(response); // TODO SUCCESS MESSAGE
     } catch {
-      console.error("didn't add run"); // TODO
+      console.error("didn't add run"); // TODO FAIL MESSAGE
     }
   }
 </script>
@@ -39,11 +39,31 @@
     border-radius: 3px;
     display: grid;
     grid-template-columns: 1fr 1fr;
+    gap: 1rem;
+    width: 50%;
+    text-align: left;
   }
 
   .full-width {
     grid-column: 1 / span 2;
     width: 100%;
+  }
+
+  input,
+  textarea {
+    border: none;
+    border-bottom: 2px solid lightblue;
+  }
+
+  .narrow {
+    width: 20%;
+  }
+
+  button {
+    background-color: lightblue;
+    border-radius: 3px;
+    align-self: center;
+    justify-self: center;
   }
 </style>
 
@@ -70,9 +90,21 @@
 
   <div>
     <label for="durationHourInput">Duration:</label>
-    <input type="number" id="durationHourInput" bind:value={hourDuration} />
-    <input type="number" id="durationMinuteInput" bind:value={minuteDuration} />
-    <input type="number" id="durationSecondInput" bind:value={secondDuration} />
+    <input
+      type="number"
+      id="durationHourInput"
+      bind:value={hourDuration}
+      class="narrow" />
+    <input
+      type="number"
+      id="durationMinuteInput"
+      bind:value={minuteDuration}
+      class="narrow" />
+    <input
+      type="number"
+      id="durationSecondInput"
+      bind:value={secondDuration}
+      class="narrow" />
   </div>
 
   <div>
@@ -82,8 +114,16 @@
 
   <div>
     <label for="paceInput">Avg pace (min mile):</label>
-    <input type="number" id="paceMinuteInput" bind:value={avgPaceMin} />
-    <input type="number" id="paceSecondInput" bind:value={avgPaceSec} />
+    <input
+      type="number"
+      id="paceMinuteInput"
+      bind:value={avgPaceMin}
+      class="narrow" />
+    <input
+      type="number"
+      id="paceSecondInput"
+      bind:value={avgPaceSec}
+      class="narrow" />
   </div>
 
   <div>
